73601386bf69f2a44bd67791e6779e53ddb05813
spencerconnelly <spenconnelly@gmail.com>
2020-02-20 18:49:18 -0500

    Initial commit. Setup eslint. Setup test environment. Remove favicon and icons. Edit index.html.

---
+++ src/App.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+    let wrapper;
+    let component;
+
+    beforeEach(() => {
+        wrapper = mount(<App />);
+    });
+
+    it('should render', () => {
+        component = wrapper.find(App);
+
+        expect(component.exists()).toBeTruthy();
+    });
+});
