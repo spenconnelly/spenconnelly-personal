8696000475520363106671dbc58d6f7036b06d74
Spencer Connelly <spenconnelly@gmail.com>
2020-06-16 20:06:05 -0400

    Updates

---
+++ precache-manifest.2f724c0f6c2d630067980acca045088d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682aa56209426234c61fc678eefb9c87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fedc09ab43ef2d8c4975",
+    "url": "/static/js/2.dbcae5fa.chunk.js"
+  },
+  {
+    "revision": "4a3c744d0703abaad1c2d70c5586fb4d",
+    "url": "/static/js/2.dbcae5fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a935ce473a34bcacee",
+    "url": "/static/js/3.8aa0b624.chunk.js"
+  },
+  {
+    "revision": "3041b3e6f2c0596d49b4",
+    "url": "/static/js/4.03caede9.chunk.js"
+  },
+  {
+    "revision": "7d438bb03d159c5694d6",
+    "url": "/static/js/main.8238810b.chunk.js"
+  },
+  {
+    "revision": "f643583d3518130c263b",
+    "url": "/static/js/runtime-main.f983b09b.js"
+  },
+  {
+    "revision": "86140b7452c1672ff332ec64d502ca7a",
+    "url": "/static/media/faceshot-close.86140b74.jpeg"
+  },
+  {
+    "revision": "06a3dc23120b064681ebee28fa074188",
+    "url": "/static/media/faceshot-landscape-large.06a3dc23.jpeg"
+  }
+]);
